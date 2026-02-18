add0698d8d0d7160be97e0987f7039a79558ddd8
Tomasz Kowalczyk <tomek@kownet.info>
2026-02-18 15:51:36 +0100

    deployment fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
   adapter: cloudflare({
+    imageService: 'cloudflare',
     workerEntryPoint: {
       path: 'src/worker.ts',
     },
