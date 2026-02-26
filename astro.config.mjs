442c68cb547178a4283c6cfee661e057d591c0f3
Tomasz Kowalczyk <tomek@kownet.info>
2026-02-26 12:58:48 +0100

    google search console fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
+  trailingSlash: 'never',
   adapter: cloudflare({
     imageService: 'cloudflare',
     workerEntryPoint: {
